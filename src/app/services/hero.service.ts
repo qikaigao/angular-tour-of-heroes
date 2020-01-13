3251041526340a6fc319ec2f75180c8234e01e06
qikaigao <qikaigao2017@gmail.com>
2020-01-13 14:05:30 -0800

    dashboard

---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Hero } from '../shared/hero';
+import { HEROES } from '../shared/mock-heroes';
+
+import { MessageService } from './message.service';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
